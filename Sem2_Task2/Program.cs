a0a6f98c870d0494420ff7daf3d10e661e9bbdf7
dubrale <alekseydubr@gmail.com>
2022-05-30 17:42:38 +0300

    Sem2

---
+++ Sem2_Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿/*Напишите программу, которая выводит третью 
+цифру заданного числа или сообщает, что третьей цифры 
+нет.*/
+Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+if (number.Length > 2)
+    Console.WriteLine($"третья цифра -> {number[2]}");
+else
+    Console.WriteLine($"число {number} слишком мало");
---
+++ Sem2_Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Напишите программу, которая принимает на 
+вход цифру, обозначающую день недели, и проверяет, 
+является ли этот день выходным*/
+Console.WriteLine("Введите номер дня недели");
+int dayNumber = int.Parse(Console.ReadLine());
+if (dayNumber > 0 && dayNumber < 8)
+    if (dayNumber > 0 && dayNumber < 6)
+        Console.WriteLine("Это будний день");
+    else
+        Console.WriteLine("Это выходной день");
+else
+    Console.WriteLine("Дней всего 7");
