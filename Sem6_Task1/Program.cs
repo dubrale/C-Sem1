4fdd9b47800dc19d3ed1b76fd55fb951e934aed1
dubrale <alekseydubr@gmail.com>
2022-06-16 19:56:56 +0300

    Sem6

---
+++ Sem6_Task1/Program.cs
@@ -0,0 +1,71 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3*/
+
+int GetHowManyNumbers(string msg)
+{
+    while (true)
+    {
+        Console.WriteLine(msg);
+        string ValueFromConsole = Console.ReadLine();
+        if (int.TryParse(ValueFromConsole, out int number) && number > 0)
+            return number;
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Это не число или число меньше 0");
+            Console.ResetColor();
+        }
+    }
+}
+int GetNumber(string msg)
+{
+    while (true)
+    {
+        Console.WriteLine(msg);
+        string ValueFromConsole = Console.ReadLine();
+        if (int.TryParse(ValueFromConsole, out int number))
+            return number;
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Это не число");
+            Console.ResetColor();
+        }
+    }
+}
+int CountPositiveFromNNumbers(int n, int countPos, int step)
+{
+    if (n != 0)
+    {
+        int number = GetNumber($"Введите {step}-ое число");
+        if (number > 0)
+            return CountPositiveFromNNumbers(--n, ++countPos, ++step);
+        else
+        {
+            return CountPositiveFromNNumbers(--n, countPos, ++step);   
+        } 
+    }
+    else
+    {
+        return countPos;
+    }
+}
+int myN = GetHowManyNumbers("Сколько чисел будете вводить?");
+Console.WriteLine();
+Console.WriteLine($"Среди введенных чисел положительных: {CountPositiveFromNNumbers(myN,0,1)}");
+// Вариант без рекурсии (заменить строчки с ... по ... )
+// int CountPositiveFromNNumbers(int n)
+// {
+//     int count = 0;
+//     for (int i = 1; i <= n; i++)
+//     {
+//         int number = GetNumber($"Введите {i}-ое число");
+//         if (number > 0)
+//             count++;
+//     }
+//     Console.WriteLine($"Из них положительных: {count}");
+// }
+// int myN = GetHowManyNumbers("Сколько чисел будете вводить?");
+// Console.WriteLine();
+// CountPositiveFromNNumbers(myN);
---
+++ Sem6_Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)*/
+int GetNumber(string msg)
+{
+    while (true)
+    {
+        Console.WriteLine(msg);
+        string ValueFromConsole = Console.ReadLine();
+        if (int.TryParse(ValueFromConsole, out int number))
+            return number;
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Это не число");
+            Console.ResetColor();
+        }
+    }
+}
+void FindIntersection(int k1, int b1, int k2, int b2)
+{
+    
+    if (k1 == k2)
+        Console.WriteLine($"Прямые y=({k1})x+({b1}) и y=({k2})x+({b2}) параллельны");
+    else
+    {
+        int x = (b2-b1)/(k1-k2);
+        int y = (b1*k2-b2*k1)/(k2-k1);
+        Console.WriteLine($"Точка пересечения прямых y=({k1})x+({b1}) и y=({k2})x+({b2}): ({x},{y})");
+    }
+    
+}
+int myK1 = GetNumber("Задайте k1");
+int myB1 = GetNumber("Задайте b1");
+int myK2 = GetNumber("Задайте k2");
+int myB2 = GetNumber("Задайте b1");
+FindIntersection(myK1,myB1,myK2,myB2);
